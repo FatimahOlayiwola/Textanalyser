836ca09b96281b0708ebf6aa0b3fe2c93ff7e61b
Olayiwola Fatimah <fatimaholayiwola02@gmail.com>
2022-01-10 11:29:27 +0100

    More on wordcount

--- js/scripts.js
+++ js/scripts.js
@@ -5,19 +5,19 @@ function noInputtedWord(word, text) {
 }
 
 //Business Logic
-// function wordCounter(text) {
-//   if (text.trim().length === 0) {
-//     return 0;
-//   }
-//   let wordCount = 0;
-//   const wordArray = text.split(" ");
-//   wordArray.forEach(function(element){
-//     if (!Number(element)) {
-//       wordCount++;
-//     };
-//   });
-//   return wordCount;
-// }
+function wordCounter(text) {
+  if (text.trim().length === 0) {
+    return 0;
+  }
+  let wordCount = 0;
+  const wordArray = text.split(" ");
+  wordArray.forEach(function(element){
+    if (!Number(element)) {
+      wordCount++;
+    };
+  });
+  return wordCount;
+}
 
 // function numberOfOccurencesInText(word, text) {
 //   if (noInputtedWord(word, text)) {
